451dad1a9ffb8635fd35bfe384d0a122707fbfe3
Erick Felipe Vieira da Silva <erick.silva@mttechne.com.br>
2023-08-31 16:40:25 -0300

    fix chart

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,68 +1,12 @@
-"use client";
+import { BarChart } from "@mui/x-charts/BarChart";
 
-import { BarPlot } from "@mui/x-charts/BarChart";
-import { ChartContainer } from "@mui/x-charts/ChartContainer";
-import { LinePlot } from "@mui/x-charts/LineChart";
-
-import { ChartsXAxis } from "@mui/x-charts/ChartsXAxis";
-import { ChartsYAxis } from "@mui/x-charts/ChartsYAxis";
-
-const series = [
-  {
-    type: "bar",
-    stack: "A",
-    yAxisKey: "qtd",
-    data: [1500, 1250, 1000],
-  },
-  {
-    type: "bar",
-    stack: "A",
-    yAxisKey: "qtd",
-    data: [350, 250, 930],
-  },
-  {
-    type: "line",
-    yAxisKey: "valor",
-    color: "red",
-    data: [1500, 1250, 2000],
-  },
-];
-
-export default function Combining() {
+export default function BasicBars() {
   return (
-    <ChartContainer
-      series={series}
-      width={530}
+    <BarChart
+      xAxis={[{ scaleType: "band", data: ["group A", "group B", "group C"] }]}
+      series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
+      width={500}
       height={300}
-      xAxis={[
-        {
-          id: "ano",
-          data: ["2017", "2020", "2023"],
-          scaleType: "band",
-          valueFormatter: (value) => value.toString(),
-        },
-      ]}
-      yAxis={[
-        {
-          id: "qtd",
-          scaleType: "linear",
-        },
-        {
-          id: "valor",
-          scaleType: "linear",
-        },
-      ]}
-      sx={{
-        backgroundColor: "#f1f5f9",
-        borderRadius: "1rem",
-        padding: "1rem",
-      }}
-    >
-      <BarPlot />
-      <LinePlot />
-      <ChartsXAxis label="Ano" position="bottom" axisId="ano" />
-      <ChartsYAxis label="Quantidade Vendas" position="left" axisId="qtd" />
-      <ChartsYAxis label="Valor" position="right" axisId="valor" />
-    </ChartContainer>
+    />
   );
 }
